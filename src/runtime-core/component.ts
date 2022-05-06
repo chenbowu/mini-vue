414830aa1edb0355070fdcb6cd71198cf047a9a5
chenbowu <1028505817@qq.com>
2022-05-07 01:14:11 +0800

    feat: 实现初始化 component & element

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,39 @@
+export function setupComponent(instance: any) {
+    // TODO 后面再实现
+    // init props
+    // init slots
+    setupStatefulComponent(instance);
+}
+
+function setupStatefulComponent(instance: any) {
+    // 获取组件配置
+    // 组件已经被转成 vnode，type 就是组件的配置
+    const Component = instance.type;
+    const { setup } = Component;
+    // 使用者可能不会写 setup，所以这里需要做判断
+    if (setup) {
+        // setup 允许返回 function & object
+        // 如果返回的是 function 那就是一个 render 函数
+        // 如果返回的是 object 那将这个 object 注入进组件实例的上下文中
+        const setupResult = setup();
+        handleSetupResult(instance, setupResult);
+    }
+}
+
+function handleSetupResult(instance, setupResult: any) {
+    if (setupResult instanceof Object) {
+        instance.state = setupResult;
+    } else if (setupResult instanceof Function) {
+
+    }
+    // 设置 render
+    finishSetupComponent(instance);
+}
+
+function finishSetupComponent(instance: any) {
+    const Component = instance.type;
+    // 将组件上的 render 函数,挂载到实例上
+    if (Component.render) {
+        instance.render = Component.render;
+    }
+}
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared";
+import { setupComponent } from "./component";
 
 export function render(vnode: any, container: any) {
     // 直接调用 patch 方法
@@ -7,24 +8,55 @@ export function render(vnode: any, container: any) {
 function patch(vnode: any, container: any) {
     // 判断是 vnode 类型是 component 还是 element
     if (typeof vnode.type === "string") {
-        processElement(vnode);
+        processElement(vnode, container);
     } else if (isObject(vnode.type)) {
-        processComponent(vnode);
+        processComponent(vnode, container);
     }
 }
 
-function processElement(vnode: any) {
-    throw new Error("Function not implemented.");
+function processElement(vnode: any, container) {
+    mountElement(vnode, container);
 }
 
-function processComponent(vnode: any) {
+function mountElement(vnode: any, container: any) {
+    const el = document.createElement(vnode.type);
+    for (let key in vnode.props) {
+        el.setAttribute(key, vnode.props[key]);
+    }
+
+    if (typeof vnode.children === "string") {
+        el.textContent = vnode.children;
+    } else if (Array.isArray(vnode.children)) {
+        mountChildren(vnode, el);
+    };
+    container.append(el);
+}
+
+function mountChildren(vnode: any, container) {
+    vnode.children.forEach(v => {
+        patch(v, container);
+    });
+}
+
+function processComponent(vnode: any, container) {
+    mountComponent(vnode, container);
+}
+
+function mountComponent(vnode, container) {
     const instance = createComponentInstance(vnode);
+    setupComponent(instance);
+    setupRenderEffect(instance, container);
 }
 
 function createComponentInstance(vnode: any) {
-    const instance = {
-        vnode
+    const component = {
+        vnode,
+        type: vnode.type
     };
-    return instance;
+    return component;
+}
+function setupRenderEffect(instance: any, container) {
+    const subTree = instance.render();
+    patch(subTree, container);
 }
 
